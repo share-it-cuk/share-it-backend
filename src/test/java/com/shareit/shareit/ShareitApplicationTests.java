f6d0f81a9ac9c891f2ff62ff5d8f5a008751e07b
Keon <lune0410k@naver.com>
2024-03-11 19:55:20 +0900

    Initial commit

---
+++ src/test/java/com/shareit/shareit/ShareitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shareit.shareit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
