287a3ce4da54c3a5f0c2f5de9aa240754334e18e
Keon <lune0410k@naver.com>
2024-04-05 22:15:24 +0900

    feat: campus condition added

--- src/main/java/com/shareit/shareit/post/repository/PostRepository.java
+++ src/main/java/com/shareit/shareit/post/repository/PostRepository.java
@@ -7,13 +7,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import com.shareit.shareit.domain.entity.Campus;
 import com.shareit.shareit.post.domain.PostType;
 import com.shareit.shareit.post.domain.entity.Post;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 	@EntityGraph(attributePaths = {"postImages"})
-	@Query("select p from Post p where p.postType=:type")
-	List<Post> findAllWithImages(@Param("type") PostType postType);
+	@Query("select p from Post p where p.postType=:type and p.campus=:campus")
+	List<Post> findAllWithImages(@Param("type") PostType postType, @Param("campus") Campus campus);
 
 }
