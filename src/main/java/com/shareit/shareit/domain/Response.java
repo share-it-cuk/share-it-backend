730c20c3bb69ed34bc7f8564c9d1a9cd03cec8f4
Keon <lune0410k@naver.com>
2024-04-29 17:06:52 +0900

    feat: response generic type change For flexible data response.

--- src/main/java/com/shareit/shareit/domain/Response.java
+++ src/main/java/com/shareit/shareit/domain/Response.java
@@ -6,16 +6,19 @@ import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class Response<T> {
 
 	private Integer status;
 	private List<String> code;
-	private List<T> data;
+	private T data;
 	private List<String> message;
 
 	public static Response<Void> ok() {
@@ -30,7 +33,7 @@ public class Response<T> {
 		Response<T> response = new Response<>();
 		response.status = HttpStatus.OK.value();
 		response.code = List.of(ResponseCode.SUCCESS.getCode());
-		response.data = List.of(data);
+		response.data = data;
 
 		return response;
 	}
