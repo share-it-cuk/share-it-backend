55d6f52ff157e66363941e3f8a00d0bd96140b6e
Keon <lune0410k@naver.com>
2024-05-02 14:34:54 +0900

    feat: security config and query dsl config added

---
+++ src/main/java/com/shareit/shareit/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.shareit.shareit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http
+			// 모든 경로 허용
+			.authorizeHttpRequests((authorizeHttpRequests)->
+				authorizeHttpRequests.anyRequest().permitAll()
+			)
+			.csrf(AbstractHttpConfigurer::disable);
+
+		return http.build();
+	}
+}
