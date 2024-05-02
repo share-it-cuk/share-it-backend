6cbd01f64659318927bf269b9b21b0f55a9488ea
Keon <lune0410k@naver.com>
2024-05-02 14:37:25 +0900

    feat: post controller added

---
+++ src/main/java/com/shareit/shareit/post/controller/PostController.java
@@ -0,0 +1,73 @@
+package com.shareit.shareit.post.controller;
+
+import java.time.LocalDateTime;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.shareit.shareit.domain.Response;
+import com.shareit.shareit.post.domain.PostType;
+import com.shareit.shareit.post.domain.dto.request.CreatePostRequest;
+import com.shareit.shareit.post.domain.dto.request.EditPostRequest;
+import com.shareit.shareit.post.domain.dto.response.PostInfoResponse;
+import com.shareit.shareit.post.domain.dto.response.PostInfoWithPaging;
+import com.shareit.shareit.post.service.PostService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class PostController {
+
+	private final PostService postService;
+
+	@PutMapping("/post/create")
+	public Response<Void> create(@RequestBody CreatePostRequest request) {
+		return postService.createPost(request);
+	}
+
+	@GetMapping("/post/all")
+	public Response<PostInfoWithPaging> searchFromAllPost(
+		@RequestParam(value = "cursorTime", required = false) LocalDateTime cursorTime,
+		@RequestParam(value = "cursorId", required = false) Long cursorId,
+		@RequestParam("limit") Integer limit,
+		@RequestParam(value = "keyword", required = false) String keyword) {
+
+		return postService.getSearchPosts(cursorTime, cursorId, limit, keyword);
+	}
+
+	@GetMapping("/post/type")
+	public Response<PostInfoWithPaging> searchFromTypePost(
+		@RequestParam(value = "cursorTime", required = false) LocalDateTime cursorTime,
+		@RequestParam(value = "cursorId", required = false) Long cursorId,
+		@RequestParam("limit") Integer limit,
+		@RequestParam(value = "keyword", required = false) String keyword, @RequestParam("postType") PostType postType) {
+
+		return postService.getSearchPostsWithFilter(cursorTime, cursorId, limit, keyword, postType);
+	}
+
+	@GetMapping("/post/{postId}")
+	public Response<PostInfoResponse> getPostDetail(@RequestParam("postId") Long postId) {
+		return postService.getPostDetail(postId);
+	}
+
+	@PutMapping("/post/update")
+	public Response<Void> updatePost(@RequestBody EditPostRequest request) {
+
+		return postService.editPost(request);
+	}
+
+	@DeleteMapping("/post/delete/{postId}")
+	public Response<Void> deletePost(@PathVariable("postId") Long postId) {
+
+		return postService.deletePost(postId);
+	}
+
+}
