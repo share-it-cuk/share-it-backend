c633a4b99468c5b64e245e2270276c2ddba41a7c
Keon <lune0410k@naver.com>
2024-05-02 14:29:50 +0900

    feat: business exception edit for message

--- src/main/java/com/shareit/shareit/Exceptions/BusinessException.java
+++ src/main/java/com/shareit/shareit/Exceptions/BusinessException.java
@@ -1,6 +1,21 @@
 package com.shareit.shareit.Exceptions;
 
+import com.shareit.shareit.domain.ResponseCode;
+
+import lombok.Getter;
+
+@Getter
 public class BusinessException extends RuntimeException{
 
+	private ResponseCode responseCode;
+
+	public BusinessException() {
+		super();
+	}
+
+	public BusinessException(ResponseCode responseCode) {
+		super(responseCode.getMessage());
+		this.responseCode = responseCode;
+	}
 
 }
