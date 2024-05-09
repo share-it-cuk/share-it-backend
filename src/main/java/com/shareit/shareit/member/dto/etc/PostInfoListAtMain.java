6bef7eeeef9aedf355c915cc1920d40463786cb2
greensnapback0229 <smdmim@gmail.com>
2024-05-09 22:36:43 +0900

    feat: add dto for response

---
+++ src/main/java/com/shareit/shareit/member/dto/etc/PostInfoListAtMain.java
@@ -0,0 +1,26 @@
+package com.shareit.shareit.member.dto.etc;
+
+import java.time.LocalDateTime;
+
+import com.shareit.shareit.post.domain.entity.Post;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PostInfoListAtMain {
+
+	private Long id;
+	private String title;
+	private Long cost;
+	private LocalDateTime updatedAt;
+
+	public static PostInfoListAtMain fromEntity(Post post) {
+		return new PostInfoListAtMain(post.getId(),
+			post.getTitle(),
+			post.getCost(),
+			post.getUpdatedAt()
+		);
+	}
+}
