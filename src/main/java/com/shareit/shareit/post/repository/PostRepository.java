e8a9203046cf0b47cd7d9183a236bf758ffeddb3
greensnapback0229 <smdmim@gmail.com>
2024-05-09 22:38:16 +0900

    feat: add query for recent posts in main page

--- src/main/java/com/shareit/shareit/post/repository/PostRepository.java
+++ src/main/java/com/shareit/shareit/post/repository/PostRepository.java
@@ -1,9 +1,11 @@
 package com.shareit.shareit.post.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.shareit.shareit.post.domain.entity.Post;
 
@@ -12,4 +14,6 @@ public interface PostRepository extends JpaRepository<Post, Long>, CustomPostRep
 	@EntityGraph(attributePaths = {"member"})
 	Optional<Post> findPostById(Long id);
 
+	@Query("SELECT p FROM Post p ORDER BY p.updatedAt DESC")
+	List<Post> findRecentTop3Post();
 }
