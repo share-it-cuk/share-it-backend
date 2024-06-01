01a101029c632fe5e6091150acb3a35863da69b8
Keon <lune0410k@naver.com>
2024-06-01 14:57:19 +0900

    feat: like dto and repo added

---
+++ src/main/java/com/shareit/shareit/like/domain/dto/LikeRequest.java
@@ -0,0 +1,14 @@
+package com.shareit.shareit.like.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LikeRequest {
+
+	private Long postId;
+
+}
