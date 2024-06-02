859ec1014ce3f5afcdd607930b26a5453f0fd885
greensnapback0229 <smdmim@gmail.com>
2024-06-02 22:06:37 +0900

    feat: add security custom filter for jwt

---
+++ src/main/java/com/shareit/shareit/security/config/SecurityConfig.java
@@ -0,0 +1,106 @@
+package com.shareit.shareit.security.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import com.shareit.shareit.security.jwt.JWTUtil;
+import com.shareit.shareit.security.jwt.JwtExceptionFilter;
+import com.shareit.shareit.security.jwt.JwtFilter;
+import com.shareit.shareit.security.oauth2.CustomSuccessHandler;
+import com.shareit.shareit.security.service.CustomOAuth2UserService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	private final CustomOAuth2UserService customOAuth2UserService;
+	private final CustomSuccessHandler customSuccessHandler;
+	private final JWTUtil jwtUtil;
+
+	public SecurityConfig(CustomOAuth2UserService customOAuth2UserService,
+		CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil) {
+		this.customSuccessHandler = customSuccessHandler;
+		this.customOAuth2UserService = customOAuth2UserService;
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() { // security를 적용하지 않을 리소스
+		return web -> web.ignoring()
+			.requestMatchers("/error", "/favicon.ico");
+	}
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+		http
+			.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+
+				@Override
+				public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+
+					CorsConfiguration configuration = new CorsConfiguration();
+
+					configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
+					configuration.setAllowedMethods(Collections.singletonList("*"));
+					configuration.setAllowCredentials(true);
+					configuration.setAllowedHeaders(Collections.singletonList("*"));
+					configuration.setMaxAge(3600L);
+
+					//configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+					configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+					return configuration;
+				}
+			}));
+
+		//csrf disable
+		http
+			.csrf((auth) -> auth.disable());
+
+		//From 로그인 방식 disable
+		http
+			.formLogin((auth) -> auth.disable());
+
+		//HTTP Basic 인증 방식 disable
+		http
+			.httpBasic((auth) -> auth.disable());
+
+		//JWTFilter 추가
+		http
+			.addFilterAfter(new JwtFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class)
+			.addFilterBefore(new JwtExceptionFilter(), JwtFilter.class);
+
+		//oauth2
+		http
+			.oauth2Login((oauth2) -> oauth2
+				.userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+					.userService(customOAuth2UserService))
+				.successHandler(customSuccessHandler)
+			);
+
+		//경로별 인가 작업
+		http
+			.authorizeHttpRequests((auth) -> auth
+				.requestMatchers("/participation/count").permitAll()
+				.requestMatchers("/auth/**").hasRole("SOCIAL")
+				.anyRequest().hasRole("USER"));
+
+		//세션 설정 : STATELESS
+		http
+			.sessionManagement((session) -> session
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+		return http.build();
+	}
+}
---
+++ src/main/java/com/shareit/shareit/security/jwt/JwtFilter.java
@@ -0,0 +1,79 @@
+package com.shareit.shareit.security.jwt;
+
+import java.io.IOException;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.shareit.shareit.security.dto.CustomOAuth2User;
+import com.shareit.shareit.security.dto.UserDto;
+
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class JwtFilter extends OncePerRequestFilter {
+
+	private final JWTUtil jwtUtil;
+
+	public JwtFilter(JWTUtil jwtUtil) {
+		this.jwtUtil = jwtUtil;
+	}
+
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+		FilterChain filterChain) throws ServletException, IOException {
+
+		System.out.println(request.getRequestURI());
+		if (request.getRequestURI().equals("/participation/count")) {
+			filterChain.doFilter(request, response);
+			return;
+		}
+
+		String authorization = null;
+
+		authorization = request.getHeader("Authorization");
+		System.out.println("[JwtFilter] - get token=" + authorization);
+
+		//Authorization 헤더 검증
+		if (authorization == null || authorization.isBlank() || authorization.isEmpty() || authorization.equals(
+			"undefined")) {
+			System.out.println("[JwtFilter] - token is blank");
+			throw new JwtException("INVALID");
+		}
+
+		//System.out.println("[filterchain] - 널검증 만료검증 사이");
+
+		//토큰
+		String token = authorization;
+		//토큰 소멸 시간 검증
+		if (jwtUtil.isExpired(token)) {
+			System.out.println("token expired");
+			throw new JwtException("EXPIRE");
+		}
+
+		//토큰에서 username과 role 획득
+		String uuid = jwtUtil.getUuid(token);
+		String role = jwtUtil.getRole(token);
+
+		//userDTO를 생성하여 값 set
+		UserDto userDTO = new UserDto();
+		userDTO.setRole(role);
+		userDTO.setUuid(uuid);
+
+		//UserDetails에 회원 정보 객체 담기
+		CustomOAuth2User customOAuth2User = new CustomOAuth2User(userDTO);
+
+		//스프링 시큐리티 인증 토큰 생성
+		Authentication authToken = new UsernamePasswordAuthenticationToken(customOAuth2User, null,
+			customOAuth2User.getAuthorities());
+
+		//세션에 사용자 등록
+		SecurityContextHolder.getContext().setAuthentication(authToken);
+		filterChain.doFilter(request, response);
+	}
+}
