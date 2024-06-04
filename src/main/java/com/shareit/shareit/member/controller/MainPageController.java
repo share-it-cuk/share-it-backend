08b9198aeabca74ee6d691fc887c57fded1e5927
greensnapback0229 <smdmim@gmail.com>
2024-06-05 04:09:59 +0900

    feat: add main page function

--- src/main/java/com/shareit/shareit/member/controller/MainPageController.java
+++ src/main/java/com/shareit/shareit/member/controller/MainPageController.java
@@ -1,24 +1,29 @@
 package com.shareit.shareit.member.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.shareit.shareit.domain.Response;
 import com.shareit.shareit.member.dto.response.AuthMainPageRes;
+import com.shareit.shareit.member.dto.response.BasicMainPageRes;
 import com.shareit.shareit.member.service.MainPageService;
 
-import lombok.RequiredArgsConstructor;
-
-@Controller
-@RequiredArgsConstructor
+@RestController
 public class MainPageController {
 
 	private final MainPageService myPageService;
 
-	@GetMapping("/main-auth")
-	public Response<AuthMainPageRes> userInfo() {
-
-		return myPageService.authMain();
+	MainPageController(MainPageService mainPageService) {
+		this.myPageService = mainPageService;
 	}
 
+	@GetMapping("/main/auth")
+	public Response<AuthMainPageRes> getAuthMain() {
+		return myPageService.getAuthMain();
+	}
+
+	@GetMapping("/main/basic")
+	public Response<BasicMainPageRes> getBasicMain() {
+		return myPageService.getBasicMain();
+	}
 }
